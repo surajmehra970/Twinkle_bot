f21f83992351dc312cb9ae8a32fbbc264228b986
Suraj <Surajmehra2000@gmail.com>
2021-05-01 10:55:30 +0530

    help upgrade

--- Messenger_Bot.py
+++ Messenger_Bot.py
@@ -41,6 +41,10 @@ async def on_message(message):
 		quote = get_quote()
 		await message.channel.send(quote)
 
+	if 'help' in msg:
+		await message.channel.send('use $help Twinkle')
+
+	
 	if any(word in msg for word in sad_words):
 		await message.channel.send(random.choice(starter_encouragements))
 
